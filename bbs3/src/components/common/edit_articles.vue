5e6663043874ba70cc30b7eeddc1518de605b7c7
bbarkthong <bbarkthong@gmail.com>
2021-10-27 16:46:06 +0900

    게시판 최초 구성

---
+++ bbs3/src/components/common/edit_articles.vue
@@ -0,0 +1,43 @@
+<template>
+  <editor
+    :initialValue="editorText"
+    :options="editorOptions"
+    height="500px"
+    initialEditType="wysiwyg"
+    previewStyle="vertical"
+  />
+</template>
+<script>
+import '@toast-ui/editor/dist/toastui-editor.css'
+import { Editor } from '@toast-ui/vue-editor'
+
+// const defaultOptions = {
+//   minHeight: '200px',
+//   language: 'ko-KR',
+//   useCommandShortcut: true,
+//   usageStatistics: true,
+//   hideModeSwitch: false,
+//   toolbarItems: [
+//     ['heading', 'bold', 'italic', 'strike'],
+//     ['hr', 'quote'],
+//     ['ul', 'ol', 'task', 'indent', 'outdent'],
+//     ['table', 'image', 'link'],
+//     ['code', 'codeblock'],
+//     ['scrollSync']
+//   ]
+// }
+
+export default {
+  components: {
+    editor: Editor
+  },
+  data () {
+    return {
+      editorText: 'This is initialValue.',
+      editorOptions: {
+        hideModeSwitch: true
+      }
+    }
+  }
+}
+</script>
